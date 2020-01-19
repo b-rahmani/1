1a8bb9aed0b17adbc8da7f2e6918f329fbe5e493
behnam <behnam.rahmani8@gmail.com>
2020-01-19 22:40:47 +0330

    slider start

--- js/custom.js
+++ js/custom.js
@@ -5,14 +5,45 @@ const reset = () => {
   iconsearch.classList.remove("shake");
 };
 
-
 const elements = {
   searchbar: document.querySelector(".searchbar"),
   iconsearch: document.querySelector(".icon-search")
 };
 
-const{ searchbar, iconsearch }=elements;
+const { searchbar, iconsearch } = elements;
 // console.log(iconsearch);
 // searchbar.addEventListener(onfocus, () => {
 //   alert("onfocused");
 // });
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
